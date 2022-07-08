8942d284050d0c0e256b2366ad74e974d424bda5
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-08 10:56:56 +0530

    creating  homePage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Home.css"
-
+import homeAnime from '../../assets/home_anime.gif'
 const Home = () => {
-    return(
+    return (
         <div>
-            <h1> I am the Home Page</h1>
+            <div className="home-container">
+                <div className="header-text">
+                    <h1>Welcome to my portfolio!</h1>
+                    <p>This is <b><i>Aditya</i></b>, a frontend developer! </p>
+                </div>
+                <div className="head-btns">
+                    <Link to="/about" className='btn btn-white'>
+                        <p className="btn-text">Know more about me </p>
+                    </Link>
+
+                    <Link to="/contact" className="btn btn-transparent">
+                        <p className="btn-text">Connect with me </p>
+                    </Link>
+                </div>
+                    <div className="splash-img">
+                        <img src={homeAnime} alt="Animation" />
+                    </div>
+            </div>
         </div>
     )
 };
