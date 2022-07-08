8a6e2bde614d706f84a5d5684153a1077c12a718
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-08 09:09:32 +0530

    add navBar and GoHome button to the app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,28 @@
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from '../Home/Home';
 import About from '../About/About'
-import Project  from '../Projects/Project';
+import Project from '../Projects/Project';
 import Skills from '../Skills/Skills';
 import Contact from '../Contact/Contact';
+import NavBar from '../NavBar/NavBar';
+import GoHome from '../GoHome/GoHome';
 
-
-
-
-const App  =  () => {
-    return(
-        <BrowserRouter>
+const App = () => {
+    return (
+        <>
+            <NavBar />
             <Routes>
-                <Route path='/' element={<Home/>} />
-                <Route path='/about' element={<About/>} />
-                <Route path='/projects' element={<Project/>} />
-                <Route path='/skills' element={<Skills/>} />
-                <Route path='/contact' element={<Contact/>} />
+                <Route path='/' exact element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/projects' element={<Project />} />
+                <Route path='/skills' element={<Skills />} />
+                <Route path='/contact' element={<Contact />} />
             </Routes>
-        </BrowserRouter>
-        )
-}
+            <GoHome />
+        </>
+    );
+};
 export default App;
