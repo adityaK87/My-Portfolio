98d8dfb558a0d759cdf954bf871b2073cffb7e3b
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-11 10:43:35 +0530

    add header and footerLink to about copmponent

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,13 +1,54 @@
 import React from 'react';
+import Header from '../Header/Header';
+import FooterLink from '../FooterLink/FooterLink';
 import "./About.css"
+import aboutVector from '../../assets/about_vector.png'
+import aboutAnime from '../../assets/about_anime.gif'
+import { Link } from 'react-router-dom';
 
 const About = () => {
-    return(
-        <div>
-            <h1>
-                I am the About me page
-            </h1>
+    return (
+        <div className='section-container'>
+            <Header
+                heading=" About Me . "
+                details=" Aditya | A student | Frontend developer !"
+            />
+
+            <div className="about-main">
+                <div className="about-main-left">
+                    {/* sub-section 1 */}
+                    <h3 className="about-sub-head">Student</h3>
+                    <p className="about-details"> I am studying about programming and Web Developement.
+                        <Link to='/projects' className='about-link-element'>check out my projects</Link></p>
+
+
+                    {/* sub-section 2 */}
+                    {/* <h3 className="about-sub-head">Student</h3>
+                    <p className="about-details"> I am studying about programming and Web Developement</p> */}
+
+                    {/* sub-section 3 */}
+                    {/* <h3 className="about-sub-head">Student</h3>
+                    <p className="about-details"> I am studying about programming and Web Developement</p> */}
+                </div>
+
+                <div className="about-main-right">
+                    <img src={aboutAnime} alt="aboutAnimeGif" className='about-anime' />
+                </div>
+            </div>
+
+            <FooterLink
+                phrase=" check out my "
+                link="projects!"
+                toAdress="/projects"
+
+            />
+
+            {/* vector Frame! */}
+
+            <div className="vector-frame">
+                <img src={aboutVector} className="about-vector" alt='about vector' />
+            </div>
         </div>
     );
 };
-export default About ;
+export default About;
