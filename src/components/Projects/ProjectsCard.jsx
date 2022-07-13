4d2719ab288adc90c13d14a8c601c991a79d0eec
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-13 10:06:44 +0530

    create projectCard component

---
+++ src/components/Projects/ProjectsCard.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Project.css'
+const ProjectsCard = ({
+    projectName,
+    projectDescription,
+    imageUrl,
+    projectUrl
+}) => {
+    return (
+        <div className="project-card">
+            <div className="image-container">
+                <a href={projectUrl} className="project-container-link">
+                    <img
+                        src={imageUrl}
+                        alt="project"
+                        className="project-image" />
+                </a>
+            </div>
+            <div className="project-detail-contsiner">
+                <h2 className="project-heading" >{projectName}</h2>
+                <p className="project-detail">{projectDescription}</p>
+            </div>
+        </div>
+    )
+}
+export default ProjectsCard;
