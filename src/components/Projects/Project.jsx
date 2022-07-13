3808069758bf6b9740c65546fb6e8d3387ac9d8a
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-13 10:08:04 +0530

    building and styling the project page

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import './Project.css';
+import Header from '../Header/Header'
+import FooterLink from '../FooterLink/FooterLink';
+import { projectsData } from '../../assets/projectsData'
+import ProjectsCard from './ProjectsCard';
 
- const Project = () => {
-    return (
-        <div>
-            <h1>I am the Projects page</h1>
-        </div>
-    );
+
+
+const Project = () => {
+  return (
+    <div className='section-container'>
+      <Header
+        heading="My Projects"
+        details="Here are a few cool things I've worked on , do check them out. "
+      />
+
+      <div className="project-card-container">
+        {
+          projectsData.map(({
+            projectName,
+            projectDescription,
+            imageUrl,
+            projectUrl
+          }) => {
+            return <ProjectsCard
+              projectName={projectName}
+              projectDescription={projectDescription}
+              imageUrl={imageUrl}
+              projectUrl={projectUrl} />
+
+          })
+        }
+      </div>
+
+      <FooterLink
+        phrase=" check out "
+        link=" my skills!"
+        toAdress="/skills"
+      />
+
+    </div>
+  );
 };
 export default Project;
