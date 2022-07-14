ea7eab27664a38ded73de8fe0e0f88b4775b933a
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-14 10:58:44 +0530

    building and styling a footer for home page

---
+++ src/components/HomeFooter/HomeFooter.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './HomeFooter.css';
+import github from '../../assets/gitLogo1.png';
+import linkedin from '../../assets/li.png';
+
+
+const HomeFooter = () => { 
+    return(
+        <div className="home-footer">
+            <div className='container'>
+                <div className="left-footer">
+                    <h1 className='my-name'>Aditya  Kumar </h1>
+                    <p className="my-details">A dedicated Front End Developer looking for challenging opportunities which could help in enhancing my skills.</p>
+                </div>
+
+                <div className="right-footer">
+                    <div className="social-media">
+                        {/* <h2>SOCIAL MEDIA</h2> */}
+                        <ul>
+                            <li> <a href="https://github.com/adityapal87"> <img src={github} alt="github" className='github' /> </a> </li>
+                            <li> <a href="https://www.linkedin.com/in/aditya-pal-526121233/"><img src={linkedin} alt="linkedin" className='linkedin'/></a> </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            
+            <div className="copyright">
+            <div class="copyright-section">© 2022 - Aditya Kumar</div>
+            </div>
+         </div> 
+        );
+};
+export default HomeFooter;
