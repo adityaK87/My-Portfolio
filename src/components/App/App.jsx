d5d93eb7ae6a0d811756e284f8d2000c4554a192
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-22 08:59:59 +0530

    add dark mode btn to app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,10 +7,11 @@ import About from '../About/About'
 import Project from '../Projects/Project';
 import Skills from '../Skills/Skills';
 import Contact from '../Contact/Contact';
-import NavBar from '../NavBar/NavBar';
+import SideBar from '../SideBar/SideBar';
 import GoHome from '../GoHome/GoHome';
 import HomeFooter from '../HomeFooter/HomeFooter';
 import { useLocation } from 'react-router-dom';
+import DarkMode from '../DarkMode/DarkMode';
 
 const App = () => {
     const [isHome, setIsHome] = useState(false);
@@ -24,16 +25,19 @@ const App = () => {
 
     return (
         <>
-            <NavBar />
-            <Routes>
-                <Route path='/' exact element={<Home/>} />
-                <Route path='/about' element={<About />} />
-                <Route path='/projects' element={<Project />} />
-                <Route path='/skills' element={<Skills />} />
-                <Route path='/contact' element={<Contact />} />
-            </Routes>
-            <GoHome />
-            { isHome && <HomeFooter/> }
+            <DarkMode/>
+            <>
+                <SideBar />
+                <Routes>
+                    <Route path='/' exact element={<Home/>} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/projects' element={<Project />} />
+                    <Route path='/skills' element={<Skills />} />
+                    <Route path='/contact' element={<Contact />} />
+                </Routes>
+                <GoHome />
+                { isHome && <HomeFooter/> }
+            </>
         </>
     );
 };
