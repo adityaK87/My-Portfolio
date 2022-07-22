5d99d18452a393d08b564923f0f79023a89eef08
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-22 09:03:05 +0530

    update contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import './Contact.css'
+import './Contact.css';
 import Header from '../Header/Header';
 import FooterLink from '../FooterLink/FooterLink';
-import contactVector from '../../assets/contact_anime.png';
-import github from '../../assets/gh.png'
-import linkedIn from '../../assets/li.png'
+import github from '../../assets/gh.png';
+import linkedIn from '../../assets/li.png';
 
 
 const Contact = () => {
     return (
         <div className='section-container'>
             <Header
-                heading="Get in touch"
-                details="Interested to collaborate? Feel free to drop me an email"
+                heading="Get in touch!"
+                details="Feel free to Contact me by submitting the form below and I will get back to you as soon as possible."
             />
-            
+
             {/* contact form container */}
             <div className="contact-form-container">
                 <form className="contact-form"
-                  action="https://formspree.io/f/xpzbgnqq"
-                  method="POST"  >
+                    action="https://formspree.io/f/xpzbgnqq"
+                    method="POST"  >
                     <input
                         type="email"
                         placeholder='Your Email Id'
                         name='email'
-                        className='input-box email-input'
+                        className='input-box email-input' required
                     />
                     {/* Email body */}
                     <textarea
@@ -34,18 +33,18 @@ const Contact = () => {
                         className='input-box body-input'
                     ></textarea>
                     {/* submit button */}
-                    <button type='submit' className='contact-btn'> Send Email</button>
+                    <button type='submit' className='contact-btn input-box'> Send Email</button>
                 </form>
 
             </div>
             {/* social icon container */}
             <div className="social-icon-container">
                 <a href="https://github.com/adityapal87">
-                    <img src={github} alt="github" className='social-icon' />
+                    <img src={github} alt="github" className='social-icon github ' />
                 </a>
 
                 <a href="https://www.linkedin.com/in/aditya-pal-526121233/">
-                    <img src={linkedIn} alt="linkedIn" />
+                    <img src={linkedIn} alt="linkedIn" className='social-icon ' />
                 </a>
 
             </div>
@@ -55,13 +54,7 @@ const Contact = () => {
                 link="about me!"
                 toAdress="/about"
             />
-            <div className="vector-frame">
-                <img
-                    src={contactVector}
-                    alt="contact"
-                    className='about-vetor'
-                />
-            </div>
+
 
         </div>
     )
