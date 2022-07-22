6401811caa1e91ea4828148256be977f699c3750
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-07-22 09:57:58 +0530

    remove skillsvector

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Skills.css';
 import Header from '../Header/Header';
 import FooterLink from '../FooterLink/FooterLink';
-import skillsVector from '../../assets/skills_vector.png'
+// import skillsVector from '../../assets/skills_vector.png'
 import { skillList } from '../../assets/skillsData';
 import SkillsCard from './SkillsCard';
 
@@ -10,12 +10,12 @@ const Skills = () => {
   return (
     <div className='section-container'>
         <Header
-          heading=" My Skills"
-          details="Passionte about new technologies, I keep exploring stuff . Here's the  Tech stack I have worked with"
+          heading=" My Skills!"
+          details="Passionte about new technologies, I keep exploring stuff. Here's the  Tech stack I have worked with."
         />
       <div className="skill-card-container">
           {
-            skillList.map(({ skillName, skillUrl }) => <SkillsCard skillName={skillName} skillUrl={skillUrl} />
+            skillList.map(({ skillName, skillUrl }) => <SkillsCard key={skillName} skillName={skillName} skillUrl={skillUrl} />
             )
           }
        </div>
@@ -25,12 +25,12 @@ const Skills = () => {
           link="touch!"
           toAdress="/contact"
         />
-      <div className="skills-vector-frame">
+      {/* <div className="skills-vector-frame">
           <img
             src={skillsVector}
             alt="skills"
             className='skills-vector' />
-      </div>
+      </div> */}
     </div>
   )
 }
