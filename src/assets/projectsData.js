605cee8a699df9d2d6d54fcde4aec438f285d715
Aditya kumar <adityapal8763244@gmail.com>
2022-10-10 20:34:10 +0530

    add projects screenshots

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -5,6 +5,7 @@ export const projectsData = [
             'Do you find it difficult to name your projects or ventures? Fear not! Check out this cool name suggestion application which will help you find the best name for your next project, venture, or startup. Keep building!',
         imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj1.PNG',
         projectUrl: 'https://name-it-any.herokuapp.com/',
+        codeUrl: "https://github.com/adityapal87/Nameit",
     },
     {
         projectName: 'Typing-test',
@@ -12,6 +13,7 @@ export const projectsData = [
             'Having a good typing speed will always help you as a developer! Here, I present you the typing speed test application, where you can check your typing speed (in wpm) and share the results with your friends too.',
         imageUrl: 'https://madhavbahl.tech/portfolio-assets/proj2.PNG',
         projectUrl: 'http://theleanprogrammer.com/flashtype/',
+        codeUrl: "https://github.com/adityapal87/Typing-test",
     },
     {
         projectName: 'PokemonSpace!',
@@ -19,6 +21,7 @@ export const projectsData = [
             "Pokemon was a huge part of our childhood, wasn't it? Talking about pokemon brings back so many memories, but do you remember the details of each pokemon? Probably No! So here I bring you a pokedex to check all pokemons :)",
         imageUrl: 'https://i.ibb.co/mJz2kS7/poke-Monline.png',
         projectUrl: 'https://pokemonspace.netlify.app/',
+        codeUrl: "https://github.com/adityapal87/PokemonSpace",
     },
     {
         projectName: 'Portfolio Website',
@@ -26,5 +29,6 @@ export const projectsData = [
             'As a developer/designer, We should definitely have our own portfolio website.So this is My Portfolio Website..',
         imageUrl: 'https://i.ibb.co/V2f4GRd/Screenshot-1.png',
         projectUrl: 'https://adityapal.netlify.app/',
+        codeUrl: "https://github.com/adityapal87/My-Portfolio",
     },
 ];
