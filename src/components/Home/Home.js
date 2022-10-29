6567cde5ac5e436f5cbc7d179cc06a8d56439edc
Aditya kumar <adityapal8763244@gmail.com>
2022-10-29 22:56:19 +0530

    add new font

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <div className="home-container">
                 <div className="header-text">
                     <h1>Welcome to my portfolio!</h1>
-                    <p>This is <b><i>Aditya</i></b>, a frontend developer! </p>
+                    <p>This is <span className='frontName'> <b><i>Aditya</i></b> </span>, a frontend developer! </p>
                 </div>
                 <div className="head-btns">
                     <Link to="/about" className='btn btn-white'>
@@ -20,7 +20,7 @@ const Home = () => {
                     </Link>
                 </div>
                 <div className="splash-img">
-                    <img src={homeAnime} alt="Animation"  className='home-anime'/>
+                    <img src={homeAnime} alt="Animation" className='home-anime' />
                 </div>
             </div>
         </div>
--- src/components/HomeFooter/HomeFooter.js
+++ src/components/HomeFooter/HomeFooter.js
@@ -25,9 +25,20 @@ const HomeFooter = () => {
                 </div>
             </div>
 
-            <div className="copyright">
-                <div className="copyright-section">© {new Date().getFullYear()} - Aditya Kumar</div>
+            {/* <p className='copyright-issue alignCenter'>
+                <b>No <i className="fa fa-copyright"></i> copyright issues.</b><br />
+                Feel free to copy. If you need any help, ping me !
+            </p> */}
+            <div className="large-4 columns">
+                <p className="footTitle alignCenter"> </p>
+                <h2 className='my-name'>Aditya Kumar</h2>
+                <p class="madewithlove">
+                    Made with <i className="fa fa-heart" ></i> in India
+                </p>
             </div>
+            <p className="copyright">
+                <span className="copyright-section">© {new Date().getFullYear()} - Aditya Kumar</span>
+            </p>
         </div>
     );
 };
