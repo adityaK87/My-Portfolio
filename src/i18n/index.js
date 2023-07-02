db71f393d33cc431fcfc76c371b622c7811e74d1
Aditya Kumar <adityapal1409@gmail.com>
2023-07-03 00:45:33 +0530

    feat : add multilingual feature

---
+++ src/i18n/index.js
@@ -0,0 +1,34 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import translationsInEng from "../locales/en/translation.json";
+import translationsInGerman from "../locales/de/translation.json";
+import translationsInItalian from "../locales/it/translation.json";
+
+// the translations
+const resources = {
+	en: {
+		translation: translationsInEng,
+	},
+	de: {
+		translation: translationsInGerman,
+	},
+	it: {
+		translation: translationsInItalian,
+	},
+};
+
+i18n.use(initReactI18next) // passes i18n down to react-i18next
+	.init({
+		resources, // resources are important to load translations for the languages.
+		lng: "it", // It acts as default language. When the site loads, content is shown in this language.
+		debug: true,
+		fallbackLng: "de", // use de if selected language is not available
+		interpolation: {
+			escapeValue: false,
+		},
+		ns: "translation", // namespaces help to divide huge translations into multiple small files.
+		defaultNS: "translation",
+	});
+
+export default i18n;
