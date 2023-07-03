19ccdbb25caf95e63bc1cc905fb39d886f67415f
Aditya Kumar <adityapal1409@gmail.com>
2023-07-03 14:20:02 +0530

    bug resolved while refreshing the page, language is also changed

--- src/components/LanguageSelector/LanguageSelector.js
+++ src/components/LanguageSelector/LanguageSelector.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import i18n from "../../i18n/index";
+import "./LanguageSelector.css";
 
 const LanguageSelector = () => {
 	const [selectedLanguage, setSelectedLanguage] = useState(i18n.language); // i18n.language contains the language assigned to lng in i18n.js file.
@@ -8,13 +9,23 @@ const LanguageSelector = () => {
 		e.preventDefault();
 		i18n.changeLanguage(e.target.value); // i18n.changeLanguage() is used to change the language assigned to lng in i18n.js file.
 		setSelectedLanguage(e.target.value);
+		localStorage.setItem("lang", e.target.value);
 	};
 
 	return (
-		<select defaultValue={selectedLanguage} onChange={chooseLanguage}>
-			<option value='de'>German</option>
-			<option value='en'>English</option>
-			<option value='it'>Italian</option>
+		<select
+			className='dropdown '
+			defaultValue={selectedLanguage}
+			onChange={chooseLanguage}>
+			<option className='dropdown-content' value='de'>
+				German
+			</option>
+			<option className='dropdown-content' value='en'>
+				English
+			</option>
+			<option className='dropdown-content' value='it'>
+				Italian
+			</option>
 		</select>
 	);
 };
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -21,7 +21,7 @@ const resources = {
 i18n.use(initReactI18next) // passes i18n down to react-i18next
 	.init({
 		resources, // resources are important to load translations for the languages.
-		lng: "it", // It acts as default language. When the site loads, content is shown in this language.
+		lng: localStorage.getItem("lang"), // It acts as default language. When the site loads, content is shown in this language.
 		debug: true,
 		fallbackLng: "de", // use de if selected language is not available
 		interpolation: {
