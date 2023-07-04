4f0369df0a06c5fcf15854f8adfe417de65864bc
Aditya Kumar <adityapal1409@gmail.com>
2023-07-04 21:38:29 +0530

    add new skills

--- src/assets/skillsData.js
+++ src/assets/skillsData.js
@@ -1,36 +1,47 @@
 export const skillList = [
-    {
-        skillName: 'HTML',
-        skillUrl: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg',
-    },
-    {
-        skillName: 'CSS',
-        skillUrl: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg',
-    },
-    {
-        skillName: 'JavaScript',
-        skillUrl: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg',
-    },
-    {
-        skillName: 'React',
-        skillUrl: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg',
-    },
-    {
-        skillName: 'Scss',
-        skillUrl: 'https://sass-lang.com/assets/img/logos/logo-b6e1ef6e.svg',
-    },
+	{
+		skillName: "React",
+		skillUrl:
+			"https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg",
+	},
+	{
+		skillName: "JavaScript",
+		skillUrl:
+			"https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg",
+	},
+	{
+		skillName: "TypeScript",
+		skillUrl:
+			"https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/typescript/typescript-original.svg",
+	},
+	{
+		skillName: "Tailwindcss",
+		skillUrl:
+			"https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/tailwindcss/tailwindcss-plain.svg",
+	},
+	{
+		skillName: "HTML",
+		skillUrl:
+			"https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg",
+	},
+	{
+		skillName: "CSS",
+		skillUrl:
+			"https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg",
+	},
+	{
+		skillName: "Sass",
+		skillUrl:
+			"https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/sass/sass-original.svg",
+	},
 
-    // {
-    //     skillName: 'TypeScript',
-    //     skillUrl: 'https://cdn.icon-icons.com/icons2/2415/PNG/512/typescript_original_logo_icon_146317.png',
-    // },
-    {
-        skillName: 'Git',
-        skillUrl: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg'
-    },
-    // {
-    //     skillName: 'Mysql',
-    //     skillUrl: 'https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mysql-colored.svg'
-    // }
-
+	{
+		skillName: "Git",
+		skillUrl: "https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg",
+	},
+	{
+		skillName: "Webpack",
+		skillUrl:
+			"https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/webpack/webpack-original.svg",
+	},
 ];
