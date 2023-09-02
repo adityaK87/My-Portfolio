73350fc344138e09d2dc0b86f46056a33e8acd7d
Aditya Kumar <adityapal1409@gmail.com>
2023-09-02 15:43:29 +0530

    add color picker extension

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -4,40 +4,24 @@ export const ProjectData = () => {
 	const { t } = useTranslation();
 	const projectsData = [
 		{
-			projectName: "Name It!",
-			projectDescription: t("PROJECT_DESCRIPTION.NAME_IT_DESCRIPTION"),
-			imageUrl:
-				"https://raw.githubusercontent.com/adityapal87/My-Portfolio/main/src/assets/name-it.PNG",
-			projectUrl: "https://adityak87.github.io/Nameit/",
-			codeUrl: "https://github.com/adityapal87/Nameit",
-		},
-		{
-			projectName: "Typing-test",
+			projectName: "Color Picker",
 			projectDescription: t(
-				"PROJECT_DESCRIPTION.TYPING_TEST_DESCRIPTION"
+				"PROJECT_DESCRIPTION.COLOR_PICKER_DESCRIPTION"
 			),
 			imageUrl:
-				"https://raw.githubusercontent.com/adityapal87/My-Portfolio/main/src/assets/typing-test.png",
-			projectUrl: "https://adityak87.github.io/Typing-test/",
-			codeUrl: "https://github.com/adityapal87/Typing-test",
+				"https://raw.githubusercontent.com/adityaK87/Color-Picker-Chrome-Extension/main/src/static/color-picker.png",
+			projectUrl:
+				"https://chrome.google.com/webstore/detail/color-picker/odlceielnakdomibdflildnbfllnjmgk/overview",
+			codeUrl:
+				"https://github.com/adityaK87/Color-Picker-Chrome-Extension",
 		},
 		{
-			projectName: "PokemonSpace!",
-			projectDescription: t(
-				"PROJECT_DESCRIPTION.POKEMONSPACE_DESCRIPTION"
-			),
+			projectName: "Travel Saathi",
+			projectDescription: t("PROJECT_DESCRIPTION.TRAVEL_APP_DESCRIPTION"),
 			imageUrl:
-				"https://raw.githubusercontent.com/adityapal87/My-Portfolio/main/src/assets/pokemonspace.png",
-			projectUrl: "https://pokemonspace.netlify.app/",
-			codeUrl: "https://github.com/adityapal87/PokemonSpace",
-		},
-		{
-			projectName: "Portfolio Website",
-			projectDescription: t("PROJECT_DESCRIPTION.PORTFOLIO_DESCRIPTION"),
-			imageUrl:
-				"https://raw.githubusercontent.com/adityapal87/My-Portfolio/main/src/assets/portfolio.png",
-			projectUrl: "https://adityapal.netlify.app/",
-			codeUrl: "https://github.com/adityapal87/My-Portfolio",
+				"https://github.com/adityaK87/Travel-Saathi/blob/main/src/img/screenShot.png?raw=true",
+			projectUrl: "https://travel-saathi.netlify.app/",
+			codeUrl: "https://github.com/adityaK87/Travel-Saathi",
 		},
 		{
 			projectName: "Marvel-Movies-App",
@@ -50,6 +34,43 @@ export const ProjectData = () => {
 			codeUrl: "https://github.com/adityaK87/Marvel-Movies-App",
 		},
 		{
+			projectName: "Portfolio Website",
+			projectDescription: t("PROJECT_DESCRIPTION.PORTFOLIO_DESCRIPTION"),
+			imageUrl:
+				"https://raw.githubusercontent.com/adityapal87/My-Portfolio/main/src/assets/portfolio.png",
+			projectUrl: "https://adityapal.netlify.app/",
+			codeUrl: "https://github.com/adityapal87/My-Portfolio",
+		},
+		{
+			projectName: "Typing-test",
+			projectDescription: t(
+				"PROJECT_DESCRIPTION.TYPING_TEST_DESCRIPTION"
+			),
+			imageUrl:
+				"https://raw.githubusercontent.com/adityapal87/My-Portfolio/main/src/assets/typing-test.png",
+			projectUrl: "https://adityak87.github.io/Typing-test/",
+			codeUrl: "https://github.com/adityapal87/Typing-test",
+		},
+		{
+			projectName: "Name It!",
+			projectDescription: t("PROJECT_DESCRIPTION.NAME_IT_DESCRIPTION"),
+			imageUrl:
+				"https://raw.githubusercontent.com/adityapal87/My-Portfolio/main/src/assets/name-it.PNG",
+			projectUrl: "https://adityak87.github.io/Nameit/",
+			codeUrl: "https://github.com/adityapal87/Nameit",
+		},
+		{
+			projectName: "PokemonSpace!",
+			projectDescription: t(
+				"PROJECT_DESCRIPTION.POKEMONSPACE_DESCRIPTION"
+			),
+			imageUrl:
+				"https://raw.githubusercontent.com/adityapal87/My-Portfolio/main/src/assets/pokemonspace.png",
+			projectUrl: "https://pokemonspace.netlify.app/",
+			codeUrl: "https://github.com/adityapal87/PokemonSpace",
+		},
+
+		{
 			projectName: "Social-Media-Card",
 			projectDescription: t(
 				"PROJECT_DESCRIPTION.SOCIAL_MEDIA_CARD_DESCRIPTION"
@@ -58,14 +79,6 @@ export const ProjectData = () => {
 				"https://raw.githubusercontent.com/adityaK87/My-Portfolio/69d31981a4e3474bbfce5d0a6a1800161ad6681c/src/assets/social-media-app.png",
 			projectUrl: "https://adityaprofiles.vercel.app/",
 			codeUrl: "https://github.com/adityaK87/social-media-card",
-		},
-		{
-			projectName: "Travel Saathi",
-			projectDescription: t("PROJECT_DESCRIPTION.TRAVEL_APP_DESCRIPTION"),
-			imageUrl:
-				"https://github.com/adityaK87/Travel-Saathi/blob/main/src/img/screenShot.png?raw=true",
-			projectUrl: "https://travel-saathi.netlify.app/",
-			codeUrl: "https://github.com/adityaK87/Travel-Saathi",
 		},
 	];
 	return projectsData;
