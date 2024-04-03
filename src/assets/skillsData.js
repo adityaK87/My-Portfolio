02596a749caf9c666a8d2bd042ac0aeea217916f
Aditya Kumar <adityapal1409@gmail.com>
2024-04-03 12:37:13 +0530

    add some new skills

--- src/assets/skillsData.js
+++ src/assets/skillsData.js
@@ -10,6 +10,26 @@ export const skillList = [
 			"https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg",
 	},
 	{
+		skillName: "NodeJS",
+		skillUrl:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+	},
+	{
+		skillName: "Express JS",
+		skillUrl:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg",
+	},
+	{
+		skillName: "MongoDB",
+		skillUrl:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+	},
+	{
+		skillName: "Redux",
+		skillUrl:
+			"https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg",
+	},
+	{
 		skillName: "TypeScript",
 		skillUrl:
 			"https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/typescript/typescript-original.svg",
